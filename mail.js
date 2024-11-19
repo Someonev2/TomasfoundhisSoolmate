64aad40af4e3833ce38514d9d5776f2e86f2f686
Someonev2 <68252400+Someonev2@users.noreply.github.com>
2024-11-19 21:20:02 +1100

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,12 +1,41 @@
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        contact : document.getElementById("contact").value,
-        attendace1 : document.getElementById("attending").value,
-        attendace2 : document.getElementById("not_attending").value,
-        plus_one1 : document.getElementById("plus_one_yes").value,
-        plus_one1 : document.getElementById("plus_one_no").value,
-        dietary : document.getElementById("dietary").value,
-    }
+const form = document.querySelector('form');
+const nameInput = document.querySelector('name');
+const contactInput = document.querySelector('contact');
+const attendingInput1 = document.querySelector('attending1');
+const attendingInput2 = document.querySelector('attending2');
+const plusInput1 = document.querySelector('plus1');
+const plusInput2 = document.querySelector('plus2');
+const dietaryInput = document.querySelector('dietary');
 
-    emailjs.send("service_cb4fygn","template_v5r98ad",parms).then(alert("Email Sent!"))
+const serviceID = 'service_tfcnu7r';
+const templateID = 'template_v5r98ad';
+const publickey = 'y5BCXyCS9i2AChyY_';
+
+emailjs.init(publickey);
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const inputData = {
+        from_name:nameInput.value,
+        user_email:contactInput.value,
+        attendace1:attendingInput1.value,
+        attendace2:attendingInput2.value,
+        plus1:plusInput1.value,
+        plus2:plusInput2.value,
+        dietary:dietaryInput.value,
+    };
+    emailjs.send(serviceID, templateID<inputData).then(()=>{
+        nameInput.value = '';
+        contactInput.value = '';
+        attendingInput1.value = '';
+        attendingInput2.value = '';
+        plusInput1.value = '';
+        plusInput2.value = '';
+        dietaryInput.value = '';
+        console.log('Sucess!');
+    },
+    (error) => {
+        console.log(error);
+    }
+);
+});
