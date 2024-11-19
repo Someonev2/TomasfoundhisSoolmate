876c03ae8b45bf7b8a2196617a36a95459e44d2b
Someonev2 <68252400+Someonev2@users.noreply.github.com>
2024-11-19 20:21:58 +1100

    Update mail.js

--- mail.js
+++ mail.js
@@ -2,10 +2,11 @@ function sendMail(){
     let parms = {
         name : document.getElementById("name").value,
         contact : document.getElementById("contact").value,
-        attendace : document.getElementByName("attendance").value,
-        plus_one : document.getElementByName("plus_one").value,
+        attendace1 : document.getElementById("attending").value,
+        attendace2 : document.getElementById("not_attending").value,
+        plus_one1 : document.getElementById("plus_one_yes").value,
+        plus_one1 : document.getElementById("plus_one_no").value,
         dietary : document.getElementById("dietary").value,
     }
 
     emailjs.send("service_cb4fygn","template_v5r98ad",parms).then(alert("Email Sent!"))
-}
