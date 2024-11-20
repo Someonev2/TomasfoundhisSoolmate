79ca0b5916700677ac91a15573102fa90ca2d11b
Someonev2 <68252400+Someonev2@users.noreply.github.com>
2024-11-20 21:39:48 +1100

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,13 +1,11 @@
 const form = document.querySelector('form');
-const nameInput = document.querySelector('name2');
-const contactInput = document.querySelector('contact');
-const attendingInput1 = document.querySelector('attending1');
-const attendingInput2 = document.querySelector('attending2');
-const plusInput1 = document.querySelector('plus1');
-const plusInput2 = document.querySelector('plus2');
+const nameInput = document.querySelector('nameuser');
+const contactInput = document.querySelector('email');
+const attendingInput = document.querySelector('input[name="attendance"]:checked').value
+const plusInput = document.querySelector('input[name="plus_one"]:checked').value
 const dietaryInput = document.querySelector('dietary');
 
-const serviceID = 'service_tfcnu7r';
+const serviceID = 'service_cb4fygn';
 const templateID = 'template_v5r98ad';
 const publickey = 'y5BCXyCS9i2AChyY_';
 
@@ -18,19 +16,16 @@ form.addEventListener('Submit RSVP', (e)=> {
     const inputData = {
         from_name:nameInput.value,
         user_email:contactInput.value,
-        attendace1:attendingInput1.value,
-        attendace2:attendingInput2.value,
-        plus1:plusInput1.value,
-        plus2:plusInput2.value,
+        attendace:attendingInput.value,
+        plus_one:plusInput.value,
         dietary:dietaryInput.value,
     };
-    emailjs.send(serviceID, templateID<inputData).then(()=>{
+    emailjs.send(serviceID, templateID<inputData).then(
+        () => {
         nameInput.value = '';
         contactInput.value = '';
-        attendingInput1.value = '';
-        attendingInput2.value = '';
-        plusInput1.value = '';
-        plusInput2.value = '';
+        attendingInput.value = '';
+        plusInput.value = '';
         dietaryInput.value = '';
         console.log('Sucess!');
     },
@@ -39,5 +34,3 @@ form.addEventListener('Submit RSVP', (e)=> {
     }
 );
 });
-
-
